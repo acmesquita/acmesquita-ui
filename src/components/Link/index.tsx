2c2d62658f3f67acd870e569e506e0eacddc12c8
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-07-07 14:45:34 -0300

    Adicionando style

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import styled from 'styled-components';
 // import { Container } from './styles';
 
 export const Link: React.FC = ({children}) => {
-  return <a href="">{children}</a>;
+  const LinkStyle = styled.a`
+    text-decoration: none;
+  `;
+
+  return <LinkStyle href="">{children}</LinkStyle>;
 }
